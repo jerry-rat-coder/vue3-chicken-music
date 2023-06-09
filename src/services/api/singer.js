8652542fcbf3c56e650ef3f7a444b496fe1dbe5d
DOC998 <3049988597@qq.com>
2023-06-09 12:56:01 +0800

    third

--- src/services/api/singer.js
+++ src/services/api/singer.js
@@ -1,9 +1,12 @@
 import {get } from '../request/index'
 
-
+const instance = axios.create({
+    baseURL: 'https://music-backend-rho.vercel.app/wyy'
+})
 
 export function getSingerList() {
-    return get('/top/artists?limit=100')
+    // return get('/top/artists?limit=100')
+    return instance.get('/top/artists')
 }
 
 export function getSingerDetail(id) {

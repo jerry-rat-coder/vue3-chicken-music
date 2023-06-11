6c3b6ce8121a96a8f35e38222b83d86467f04da7
DOC998 <3049988597@qq.com>
2023-06-11 20:44:48 +0800

    REDEPLOY

--- src/services/request/config.js
+++ src/services/request/config.js
@@ -1,5 +1,4 @@
 // export const BASE_URL = 'http://cloud-music.pl-fe.cn'
 // export const BASE_URL = 'http://localhost:3000/wyy'
-// export const BASE_URL = 'https://music-backend-rho.vercel.app/wyy'
-export const BASE_URL = 'https://music-backend-cgc7ljrum-doc998.vercel.app/wyy'
+export const BASE_URL = 'https://music-backend-rho.vercel.app/wyy'
 export const TIMEOUT = 5000
